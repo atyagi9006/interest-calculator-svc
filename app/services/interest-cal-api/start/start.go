3d29905eeab51948c269582b66f5dd963baf6440
amit <tyagi.amit9006.com>
2023-10-26 08:43:09 +0530

    added grace shutdown and mongodb support

--- app/services/interest-cal-api/start/start.go
+++ app/services/interest-cal-api/start/start.go
@@ -3,11 +3,15 @@ package start
 import (
 	"fmt"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/atyagi9006/interest-calculator-svc/app/services/interest-cal-api/handlers"
 	"github.com/atyagi9006/interest-calculator-svc/business/data"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
+	"go.mongodb.org/mongo-driver/mongo"
 )
 
 const (
@@ -29,15 +33,36 @@ func Run(build string) {
 		log.Fatal("Database Connection Error $s", err)
 	}
 	fmt.Println("Database connection success!")
-	fmt.Println("Database connection success!")
 
+	defer cancel()
+
+	app := setupApp(db)
+	// Listen from a different goroutine
+	go func() {
+		if err := app.Listen(":3500"); err != nil {
+			log.Panic(err)
+		}
+	}()
+
+	c := make(chan os.Signal, 1)                    // Create channel to signify a signal being sent
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM) // When an interrupt or termination signal is sent, notify the channel
+
+	<-c // This blocks the main thread until an interrupt is received
+	fmt.Println("Gracefully shutting down...")
+	_ = app.Shutdown()
+
+	fmt.Println("Running cleanup tasks...")
+
+	// Your cleanup tasks go here
+	// db.Close()
+	// redisConn.Close()
+	fmt.Println("Fiber was successful shutdown.")
+
+}
+
+func setupApp(db *mongo.Database) *fiber.App {
 	app := fiber.New()
 	app.Use(cors.New())
 	handlers.V1(app, db, collection)
-	defer cancel()
-
-	err = app.Listen(":3500")
-	if err != nil {
-		log.Fatal(err)
-	}
+	return app
 }
